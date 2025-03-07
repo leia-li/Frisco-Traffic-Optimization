3e0ccf6e9412e0f6e1084e62e3d4d8b175ea3bd4
Leia Sing <32993345+leia-sing@users.noreply.github.com>
2025-03-07 17:25:48 -0600

    Update traffic_volume_views.sql

--- traffic_volume_views.sql
+++ traffic_volume_views.sql
@@ -1,3 +1,39 @@
+-- Validate May 15 665 signalID extract in MySQL
+-- 1. Validation test for May 15 665 data
+-- 2. Create temporary table for 5 minute view
+-- 3. Create calculated traffic volume based on approach view
+
+
+-- Validation test for import of May 15 665 data
+SELECT COUNT(*)
+FROM frisco_traffic
+WHERE date='2024-05-15' AND signalid = 665;
+-- count: 175129
+
+-- Create view for 5 minute intervals
+CREATE VIEW may15_signal665_5min AS
+SELECT 
+FLOOR(timestamp / 300000) AS interval_no_5m, ## creates 5-min intervals
+eventparam AS approach,
+COUNT(eventparam) AS volume -- tallies number of cars detected from a certain approach
+FROM 
+    frisco_traffic
+WHERE 
+    date='2024-05-15'
+    ## signalID of 665
+    AND signalid = 665
+    ## approach numbers
+    AND eventparam IN (58, 59, 60, 61, 62, 64, 40, 38, 37, 36, 35, 34, 48, 46, 45, 44, 43, 42, 56, 54, 53, 52, 51, 50)
+GROUP BY
+    timestamp, eventparam -- grouping ensures that cars are tallied by approach for a particular bucket/interval of time
+ORDER BY timestamp ASC;
+
+-- validation test for 5 minute view
+SELECT COUNT(*)
+FROM may15_signal665_5min;
+-- result: 70849
+
+-- calculate traffic flow by 5 min interval using view
 SELECT
     interval_no_5m,
     SUM(IF(approach IN (58, 59),volume,0)) AS EBLT,
